47b0121f526c44dee155398245f577406c6909df
felavor <fellipe.lavor@sptech.school>
2025-05-19 20:04:21 -0300

    Cadastro completo!

--- banco_pro_ind/banco projeto individual.sql
+++ banco_pro_ind/banco projeto individual.sql
@@ -1,17 +1,18 @@
 create database projeto_individual;
-
 use projeto_individual;
 
-create table usuario (
-id		int primary key auto_increment,
-nome	varchar(50),
-email	varchar(100),
-senha	varchar(30)
+CREATE TABLE usuario (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	nome VARCHAR(50),
+	email VARCHAR(50),
+	senha VARCHAR(50)
 );
 
-create table quiz (
-id			int primary key auto_increment,
-pontuacao	int,
-dataHora	timestamp default current_timestamp
-) auto_increment = 100;
-
+CREATE TABLE quiz (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    titulo VARCHAR(150) NOT NULL,
+    descricao TEXT,
+    data_criacao DATETIME DEFAULT CURRENT_TIMESTAMP,
+    perguntas TEXT,
+    correta BOOLEAN NOT NULL DEFAULT FALSE
+);
