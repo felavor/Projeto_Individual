48bebf390c8052607fda8d7fd4a23eac3baab0c6
felavor <fellipe.lavor@sptech.school>
2025-06-01 08:45:52 -0300

    alteração banco

--- banco_pro_ind/banco projeto individual.sql
+++ banco_pro_ind/banco projeto individual.sql
@@ -10,18 +10,33 @@ CREATE TABLE usuario (
 
 CREATE TABLE quiz (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    titulo VARCHAR(150) NOT NULL,
     descricao TEXT,
     data_criacao DATETIME DEFAULT CURRENT_TIMESTAMP,
-    perguntas TEXT,
-    correta BOOLEAN NOT NULL DEFAULT FALSE
+    perguntas TEXT
 );
+
+INSERT INTO quiz (descricao, perguntas) VALUES
+('Quiz sobre Veigh', 'Qual dessas músicas, pertence ao Álbum Dos Prédios?'),
+('Quiz sobre Veigh', 'Qual dessas músicas, pertence ao Álbum Dos Prédios Deluxe?'),
+('Quiz sobre Veigh', 'Qual estilo musical predomina nas músicas do Veigh?'),
+('Quiz sobre Veigh', 'Qual o nome de sua gravadora?'),
+('Quiz sobre Veigh', 'O Veigh possui aproximadamente, quantos ouvintes mensais?'),
+('Quiz sobre Veigh', 'Com qual artista internacional, Veigh realizou seu primeiro ft.'),
+('Quiz sobre Veigh', 'Veigh já fez uma música relacionada um time de e-Sports, qual time é esse?'),
+('Quiz sobre Veigh', 'A música Os mlk é ruim pertence à qual tipo musical?'),
+('Quiz sobre Veigh', 'De quais álbuns abaixo o Veigh NÃO participou?'),
+('Quiz sobre Veigh', 'Qual o nome da mãe do Veigh?');
+
 
 CREATE TABLE respostas_usuarios (
     fkUsuario INT,
     fkQuiz INT,
-    respostas_certas INT,
+    repostas_certas INT,
     primary key (fkUsuario, fkQuiz),
     foreign key (fkUsuario) references usuario(id),
     foreign key (fkQuiz) references quiz(id)
 );
+
+select * from usuario;
+select * from quiz;
+select * from respostas_usuarios;
